a582a0045cd8867c9d1a0e577132723ce9524eb2
lishixiang <1600923223@qq.com>
2020-03-17 16:22:59 +0800

    finish progressbar work

--- app/src/main/java/com/kotlin/github_test/ui/login/LoginActivity.kt
+++ app/src/main/java/com/kotlin/github_test/ui/login/LoginActivity.kt
@@ -1,9 +1,10 @@
 package com.kotlin.github_test.ui.login
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import com.kotlin.common.ext.otherWise
 import com.kotlin.common.ext.yes
 import com.kotlin.github_test.R
@@ -15,6 +16,8 @@ class LoginActivity : BaseActivity<LoginPresenter>() {
     val signInButton by lazy{findViewById<Button>(R.id.signInButton)}
     val password by lazy { findViewById<EditText>(R.id.password) }
     val username by lazy { findViewById<EditText>(R.id.username) }
+    val progressBar by lazy{findViewById<ProgressBar>(R.id.progressBar)}
+    val logForm by lazy{findViewById<LinearLayout>(R.id.logForm)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,18 +38,39 @@ class LoginActivity : BaseActivity<LoginPresenter>() {
         }
     }
 
+    private fun showProgressbar(isShow:Boolean){
+        var shortAnimTime = resources.getInteger(android.R.integer.config_shortAnimTime)
+        logForm.animate().setDuration(shortAnimTime.toLong()).alpha((if(isShow) 0 else 1).toFloat())
+            .setListener(object :AnimatorListenerAdapter(){
+                override fun onAnimationEnd(animation: Animator?) {
+                    logForm.visibility = if(isShow) View.GONE else View.VISIBLE
+                }
+            })
+        progressBar.animate().setDuration(shortAnimTime.toLong()).alpha((if(isShow) 0 else 1).toFloat())
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    progressBar.visibility = if(isShow) View.VISIBLE else View.GONE
+                }
+
+            })
+    }
+
     private fun showTips(view: EditText, tips: String) {
         view.requestFocus()
         view.error = tips
     }
 
-    fun loginStart(){}
+    fun loginStart(){
+        showProgressbar(true)
+    }
     fun loginError(e:Throwable){
         e.printStackTrace()
         Toast.makeText(this,"登录失败",Toast.LENGTH_SHORT).show()
+        showProgressbar(false)
     }
     fun loginSuccess(){
         Toast.makeText(this,"登录成功",Toast.LENGTH_SHORT).show()
+        showProgressbar(false)
     }
     fun dataInit(pwd:String,name:String){
         username.setText(name)
